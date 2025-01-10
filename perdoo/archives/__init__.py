b49ce892918e718182d1559682851929298822b7
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2025-01-10 14:57:52 +1300

    Revert "Mark py7zr as a required dependency (#35)"
    This reverts commit 94b2fd3dcd99ce80bf1a6eab08dfa77c030f0bbf.

--- perdoo/archives/__init__.py
+++ perdoo/archives/__init__.py
@@ -12,7 +12,6 @@ from pathlib import Path
 from tarfile import is_tarfile
 from zipfile import is_zipfile
 
-from py7zr import is_7zfile
 from rarfile import is_rarfile
 
 from perdoo.archives._base import BaseArchive
@@ -20,6 +19,13 @@ from perdoo.archives.cb7 import CB7Archive
 from perdoo.archives.cbr import CBRArchive
 from perdoo.archives.cbt import CBTArchive
 from perdoo.archives.cbz import CBZArchive
+
+try:
+    from py7zr import is_7zfile
+
+    py7zr_loaded = True
+except ImportError:
+    py7zr_loaded = False
 
 
 def get_archive(path: Path) -> BaseArchive:
@@ -29,17 +35,15 @@ def get_archive(path: Path) -> BaseArchive:
         return CBRArchive(path=path)
     if is_tarfile(path):
         return CBTArchive(path=path)
-    if is_7zfile(path):
+    if py7zr_loaded and is_7zfile:
         return CB7Archive(path=path)
     raise NotImplementedError(f"{path.name} is an unsupported archive")
 
 
 def get_archive_class(extension: str) -> type[BaseArchive]:
-    if archive_class := {
-        "cbz": CBZArchive,
-        "cbr": CBRArchive,
-        "cbt": CBTArchive,
-        "cb7": CB7Archive,
-    }.get(extension):
-        return archive_class
+    archive_types = {"cbz": CBZArchive, "cbr": CBRArchive, "cbt": CBTArchive}
+    if extension == "cb7" and py7zr_loaded:
+        return CB7Archive
+    if extension in archive_types:
+        return archive_types[extension]
     raise NotImplementedError(f"{extension} is an unsupported archive")
